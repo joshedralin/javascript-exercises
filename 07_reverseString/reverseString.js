8f76938a650d0632131016eba50e4b98dec7d69d
Josh <joshedralin@gmail.com>
2025-05-31 13:23:43 -0700

    Finish 07_reverseString

--- 07_reverseString/reverseString.js
+++ 07_reverseString/reverseString.js
@@ -1,5 +1,8 @@
-const reverseString = function() {
-
+const reverseString = function(word) {
+    let wordArray = word.split("");
+    console.log(wordArray);
+    wordArray.reverse();
+    return wordArray.join("");
 };
 
 // Do not edit below this line
