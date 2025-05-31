637f7d38d32722defd3114c663b45bc9f3982ab8
Josh <joshedralin@gmail.com>
2025-05-31 14:51:42 -0700

    Finish 11_tempConversion

--- 11_tempConversion/tempConversion.js
+++ 11_tempConversion/tempConversion.js
@@ -1,7 +1,16 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(temp) {
+  return Math.round((temp - 32) * 5/9 * 10) / 10;
+
+  // let celcius = (temp - 32) * 5/9;
+  // celcius = parseFloat(celcius.toFixed(1));
+  // return celcius;
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(temp) {
+  return Math.round((temp * 9/5 + 32) * 10) / 10;
+
+  // let fahren = temp * 9/5 + 32;
+  // return parseFloat(fahren.toFixed(1));
 };
 
 // Do not edit below this line
