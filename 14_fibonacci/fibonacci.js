1e70dc8ca031bd2ab3d2765a8885162a760f6bc4
Josh <joshedralin@gmail.com>
2025-06-03 13:50:57 -0700

    Finish 14_fibonacci

--- 14_fibonacci/fibonacci.js
+++ 14_fibonacci/fibonacci.js
@@ -1,6 +1,19 @@
-const fibonacci = function() {
+const fibonacci = function(num) {
+    num = Number(num);
+    if (num === 0) return 0;
+    if (num < 0) return 'OOPS';
 
+    let secondPrev = 0;
+    let firstPrev = 1;
+    for (let i = 2; i <= num; i++) {
+        let current = firstPrev + secondPrev;
+        secondPrev = firstPrev;
+        firstPrev = current;
+    }
+    return firstPrev;
 };
+
+//1, 1, 2, 3, 5, 8,
 
 // Do not edit below this line
 module.exports = fibonacci;
