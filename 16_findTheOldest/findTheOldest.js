edb62d8e114823635525ec160a14c4b55d39ab36
Josh <joshedralin@gmail.com>
2025-06-03 14:43:34 -0700

    Finish 16_findTheOldest

--- 16_findTheOldest/findTheOldest.js
+++ 16_findTheOldest/findTheOldest.js
@@ -1,5 +1,12 @@
-const findTheOldest = function() {
+const findAge = (person) => {
+    const death = person.yearOfDeath ?? new Date().getFullYear();
+    return death - person.yearOfBirth;
+};
 
+const findTheOldest = function(arr) {
+    return arr.reduce((oldest, current) => {
+        return findAge(oldest) > findAge(current) ? oldest : current;
+    });
 };
 
 // Do not edit below this line
