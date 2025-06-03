ce1d562cbae3bc2f325f831533ef27c5f2c0b4b7
Josh <joshedralin@gmail.com>
2025-06-03 12:25:09 -0700

    Finish 13_palindromes

--- 13_palindromes/palindromes.js
+++ 13_palindromes/palindromes.js
@@ -1,5 +1,9 @@
-const palindromes = function () {
-
+const palindromes = function (word) {
+    const arr = word.toLowerCase().replace(/[^a-z0-9]/g, '').split("");
+    for (let i = 0; i < arr.length / 2; i++) {
+        if (arr[i] !== arr[arr.length - i - 1]) return false;
+    }
+    return true;
 };
 
 // Do not edit below this line
